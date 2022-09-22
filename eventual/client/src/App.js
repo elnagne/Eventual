4ce0309c46672318e5d46a26a3795c739e94b640
Mohamad El Kadri <mmmelkadri@gmail.com>
2022-09-22 10:15:44 -0400

    Cleaned up Client and installed react-router-dom.

--- eventual/client/src/App.js
+++ eventual/client/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// We use Route in order to define the different routes of our application
+import { Route, Routes } from "react-router-dom";
+
+// We import all the components we need in our app
+
+const App = () => {
+ return (
+   <div>
+     <button onclick="myFunction()">Click me</button>
+   </div>
+ );
+};
 
 export default App;

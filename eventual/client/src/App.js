7042caf104d9208b943225be84e2b5ce65e37bcd
Ivy Wills <ivy.wills@mail.utoronto.ca>
2022-10-07 19:17:51 -0400

    Sprint 1 Final Fixes
    Made a few changes to clean things up.

--- eventual/client/src/App.js
+++ eventual/client/src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 
 // Components
-import Home from "./components/Home";
-import Events from "./components/Events";
-import Liked from "./components/Liked";
-import History from "./components/History";
-import Settings from "./components/Settings";
-import DbWriteTemp from "./components/DbWriteTemp";
-import Register from "./components/Register";
-import LogIn from "./components/LogIn";
+import Home from './components/Home';
+import Events from './components/Events';
+import Liked from './components/Liked';
+import History from './components/History';
+import Settings from './components/Settings';
+import DbWriteTemp from './components/DbWriteTemp';
+import Register from './components/Register';
+import AddEvents from './components/AddEvent';
+import LogIn from './components/LogIn';
 // We use Route in order to define the different routes of our application
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
@@ -22,6 +23,7 @@ const App = () => {
         <Route path="/liked" element={<Liked />} />
         <Route path="/history" element={<History />} />
         <Route path="/settings" element={<Settings />} />
+        <Route path="/add-events" element={<AddEvents />} />
         <Route path="/dbwritetemp" element={<DbWriteTemp />} />
         <Route path="/register" element={<Register />} />
       </Routes>

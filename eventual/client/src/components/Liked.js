04594b88cab52856c9aecb9a65661cb65f3615ff
elnagne <40046289+elnagne@users.noreply.github.com>
2022-10-17 03:35:46 -0400

    RUN-4 Changed front and back end of Liked page to show only liked events

--- eventual/client/src/components/Liked.js
+++ eventual/client/src/components/Liked.js
@@ -1,103 +1,37 @@
 import React, { useEffect, useState } from "react";
-import Button from 'react-bootstrap/Button';
-import Sidebar from './Sidebar'
-const Events = (props) => (
- <tr>
-   <td>{props.testEvents.event_name}</td>
-   <td>{props.testEvents.description}</td>
-   <td>{props.testEvents.num_likes}</td>
-   <td>
-   <Button variant="outline-primary"
-        onClick={() => {
-          props.likeEvent(props.testEvents._id)
-        }}
-      > Like 
-    </Button>{'  '}
-   <Button variant="outline-danger"
-        onClick={() => {
-          props.dislikeEvent(props.testEvents._id)
-        }}
-      > Dislike 
-    </Button>
-   </td>
- </tr>
-);
- 
-export default function EventList() {
- const [events, setEvents] = useState([]);
- 
- useEffect(() => {
-   async function getEvents() {
-     const response = await fetch(`http://localhost:5000/liked/`);
- 
-     if (!response.ok) {
-       const message = `Oh nyo T-T: ${response.statusText}`;
-       window.alert(message);
-       return;
-     }
- 
-     const events = await response.json();
-     setEvents(events);
-   }
- 
-   getEvents();
- 
-   return;
- }, [events.length]);
+import Sidebar from "./Sidebar";
+import EventsDisplay from "./EventsDisplay";
 
- // This method increase the number of likes by 1
- async function likeEvent(id) {
-  // passes the id of the event
-  await fetch(`http://localhost:5000/liked/add_like/${id}`, {
-      method: "POST"
-    });
-  window.location.reload(false);
-}
+const LikedEvents = () => {
+  const [events, setEvents] = useState([]);
+  const logged_account_id = "632c889ad56e85f52f50ac78";
+  useEffect(() => {
+    async function getEvents() {
+      const response = await fetch(`http://localhost:5000/search/liked/${logged_account_id}`);
 
-// This method decrease the number of likes by 1
-async function dislikeEvent(id) {
-  // passes the id of the event
-  await fetch(`http://localhost:5000/liked/add_dislike/${id}`, {
-      method: "POST"
-    });
-  window.location.reload(false);
-}
-  
- // This method will map out the records on the table
- function eventsList() {
-   return events.map((testEvents) => {
-     return (
-       <Events
-       testEvents={testEvents}
-       likeEvent={() => likeEvent(testEvents._id)}
-       dislikeEvent={() => dislikeEvent(testEvents._id)}
-       key={testEvents._id}
-       />
-     );
-   });
- }
- 
- // This following section will display the table with the records of individuals.
- return (
-  <div className="eventsWrapper">
-        <Sidebar/>
-        <div className="eventContent">
-        <div>
-     <h3>Event List</h3>
-          <table className="table table-striped" style={{ marginTop: 20 }}>
-            <thead>
-              <tr>
-                <th>Event Name</th>
-                <th>Description</th>
-                <th>Number of Likes</th>
-                <th>Action</th>
-              </tr>
-            </thead>
-            <tbody>{eventsList()}</tbody>
-          </table>
-        </div>
-        </div>
+      if (!response.ok) {
+        const message = `${response.statusText}`;
+        window.alert(message);
+        return;
+      }
+
+      const events = await response.json();
+      setEvents(events);
+    }
+
+    getEvents();
+
+    return;
+  }, [events.length]);
+
+  return (
+    <div className="eventsWrapper">
+      <Sidebar />
+      <div className="eventContent">
+        <EventsDisplay events={events} />
+      </div>
     </div>
+  );
+};
 
- );
-}
+export default LikedEvents;

f1734eb306ccbd8a06f053fbe6a9b53981f4e662
Ivy Wills <ivy.wills@mail.utoronto.ca>
2022-10-21 18:16:49 -0400

    RUN-1 Added page for user events

--- eventual/client/src/components/MyEvents.js
+++ eventual/client/src/components/MyEvents.js
@@ -6,7 +6,12 @@ import { SearchContext } from './SearchContext';
 import Alert from 'react-bootstrap/Alert';
 
 const Events = () => {
-  const [events, setEvents] = useState([]);
+  const [events_authors, setEvents] = useState([]);
+
+  const [author, setAuthor] = useState(null);
+
+  const login_author = localStorage.getItem('userid');
+  // console.log(login_author);
 
   useEffect(() => {
     getEvents();
@@ -22,21 +27,34 @@ const Events = () => {
       return;
     }
 
+    const events_authors = [];
+
     const events = await response.json();
 
+    await setAuthor(localStorage.getItem('userid'));
+
+    console.log('logined author:', login_author);
+
     for (let i = 0; i < events.length; i++) {
-      console.log(events[i]);
+      // console.log('event:', events[i]);
+      // console.log('event author:', events[i].author);
+      if (events[i].author == login_author) {
+        console.log('event:', events[i]);
+        console.log('event author:', events[i].author);
+        events_authors.push(events[i]);
+        console.log('event in array:', events_authors[0]);
+      }
     }
 
-    setEvents(events);
+    setEvents(events_authors);
   }
 
   return (
     <div className="eventsWrapper">
       <Sidebar />
       <div className="eventContent">
-        {events.length > 0 ? (
-          <EventsDisplay events={events} />
+        {events_authors.length > 0 ? (
+          <EventsDisplay events={events_authors} />
         ) : (
           <Alert variant="primary" className="mx-4 my-4">
             No events found.

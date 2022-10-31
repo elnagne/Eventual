bd33e477998c7b56060f776be66ab36475cdc6a1
elnagne <40046289+elnagne@users.noreply.github.com>
2022-10-31 00:43:34 -0400

    RUN-32-Added front end for adding notifications

--- eventual/client/src/components/AddNotification.js
+++ eventual/client/src/components/AddNotification.js
@@ -8,22 +8,16 @@ import { useParams } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-const UpdateEvent = () => {
+const AddNotification = () => {
   const { id } = useParams();
   console.log(id);
   const [event, setEvent] = useState(null);
   const [event_name, setEvent_name] = useState('');
-  const [author, setAuthor] = useState(null);
+  const [event_id, setEvent_id] = useState('');
   const [description, setDescription] = useState('');
-  const [image, setImage] = useState(null);
-  const [email, setEmail] = useState('');
-  const [phone, setPhone] = useState('');
-  const [date_of_event, setDate_of_event] = useState('');
-  const [time_of_event, setTime_of_event] = useState('');
-  const [num_slots, setNum_slots] = useState(0);
-  const [woman_only, setWoman_only] = useState(false);
-  const [location, setLocation] = useState('');
-  const [category, setCategory] = useState('');
+  const [title, setTitle] = useState('');
+  const [liked_by, setLikedBy] = useState([]);
+  const [attending_users, setAttendedBy] = useState([]);
 
   const login_author = localStorage.getItem('userid');
 
@@ -45,65 +39,38 @@ const UpdateEvent = () => {
     setEvent(event);
 
     setEvent_name(event.event_name);
-    setDescription(event.description);
-    setImage(event.image_url);
-    setEmail(event.email);
-    setPhone(event.phone);
-    setDate_of_event(event.date_of_event);
-    setTime_of_event(event.time_of_event);
-    setNum_slots(event.num_slots);
-    setWoman_only(event.woman_only);
-    setLocation(event.location);
-    setCategory(event.category);
+    setEvent_id(event._id);
+    setLikedBy(event.liked_by);
+    setAttendedBy(event.attending_users);
   }
 
-  const handleCheck = () => {
-    setWoman_only(true);
-  };
 
   const onSubmit = async (e) => {
     e.preventDefault();
-    toast('You Updated Your Event!');
+    toast('Notification Added!');
     await console.log(localStorage.getItem('userid'));
-    await setAuthor(login_author);
     console.log(login_author);
-    const newEvent = {
-      event_name: event_name,
-      author: login_author,
+    const newNotif = {
+      event_id: setEvent_id,
+      title: title,
       description: description,
-      image_url: image,
-      phone: phone,
-      email: email,
-      date_of_event: date_of_event,
-      time_of_event: time_of_event,
-      num_slots: num_slots,
-      woman_only: woman_only,
-      location: location,
-      category: category,
+      liked_by: liked_by,
+      attending_users: attending_users
     };
 
-    await fetch('http://localhost:5000/testEvents/update/' + id, {
+    await fetch('http://localhost:5000/notif/add', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(newEvent),
+      body: JSON.stringify(newNotif),
     }).catch((error) => {
       window.alert(error);
       return;
     });
 
-    setEvent_name('');
     setDescription('');
-    setImage(null);
-    setEmail('');
-    setPhone('');
-    setDate_of_event('');
-    setTime_of_event('');
-    setNum_slots(0);
-    setWoman_only('');
-    setLocation('');
-    setCategory('');
+    setTitle('');
   };
 
   //   const namePlaceholder = event.hasOwnProperty('event_name')
@@ -115,14 +82,14 @@ const UpdateEvent = () => {
       <SidebarPro />
       <div className="AddEvent">
         <form
-          name="AYAYAYAYAY"
+          name="notification"
           className="AddEventForm"
           encType="multipart/form-data"
           onSubmit={onSubmit}
         >
           <br />
           <br />
-          <h2 className="EventTitle">Update Event</h2>
+          <h2 className="EventTitle">Add Notification</h2>
           <div className="AddEventElement">
             <label>Event Name</label>
             <input
@@ -132,6 +99,18 @@ const UpdateEvent = () => {
               placeholder="Name of Events"
               value={event_name}
               onChange={(e) => setEvent_name(e.target.value)}
+              disabled
+            />
+          </div>
+          <div className="AddEventElement">
+            <label>Title</label>
+            <input
+              className="form-control"
+              type="text"
+              security=""
+              placeholder="Title of the notification"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
             />
           </div>
           <div className="AddEventElement">
@@ -140,107 +119,9 @@ const UpdateEvent = () => {
               type="text"
               rows="6"
               className="form-control input-lg"
-              placeholder="Enter Description"
+              placeholder="Notification body content"
               value={description}
               onChange={(e) => setDescription(e.target.value)}
-            />
-          </div>
-          <p>
-            <label htmlFor="file">Your file:</label>{' '}
-            <Widget
-              publicKey="6092add1783f1344a4e4"
-              type="hidden"
-              role="uploadcare-uploader"
-              name="my_file"
-              id="uploadcare-file"
-              value={image}
-              onChange={(info) => {
-                console.log(info.cdnUrl);
-                setImage(info.cdnUrl);
-              }}
-            />
-          </p>
-          <div className="AddEventElement">
-            <label>Phone</label>
-            <input
-              class="form-control"
-              type="text"
-              placeholder="Enter Phone"
-              value={phone}
-              onChange={(e) => setPhone(e.target.value)}
-            />
-          </div>
-          <div className="AddEventElement">
-            <label>Email</label>
-            <input
-              class="form-control"
-              type="text"
-              placeholder="Enter Email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-          <div className="AddEventElement">
-            <label>Date</label>
-            <input
-              class="form-control"
-              type="date"
-              placeholder="Enter Date"
-              value={date_of_event}
-              onChange={(e) => setDate_of_event(e.target.value)}
-            />
-          </div>
-          <div className="AddEventElement">
-            <label>Time</label>
-            <input
-              class="form-control"
-              type="time"
-              placeholder="Enter Time"
-              value={time_of_event}
-              onChange={(e) => setTime_of_event(e.target.value)}
-            />
-          </div>
-          <div className="AddEventElement">
-            <label>Address</label>
-            <input
-              class="form-control"
-              type="location"
-              placeholder="Enter a valid address"
-              value={location}
-              onChange={(e) => setLocation(e.target.value)}
-            />
-          </div>
-          <label>Category</label>
-          <select
-            class="form-control"
-            className="AddEventElement"
-            value={category}
-            onChange={(e) => setCategory(e.target.value)}
-          >
-            <option>Sport</option>
-            <option>Entertainment</option>
-            <option>Board Game</option>
-            <option>Concert</option>
-            <option>Charity</option>
-          </select>
-
-          <div className="AddEventElement">
-            <label>awawawaw</label>
-            <input
-              class="form-control"
-              type="number"
-              placeholder="Enter Number of Slots"
-              value={num_slots}
-              onChange={(e) => setNum_slots(e.target.value)}
-            />
-          </div>
-          <div className="form-group form-check">
-            <label className="form-check-label">Woman Only?</label>
-            <input
-              type="checkbox"
-              className="form-check-input"
-              value={woman_only}
-              onChange={handleCheck}
             />
           </div>
           <Button
@@ -249,7 +130,7 @@ const UpdateEvent = () => {
             type="submit"
             value="send"
           >
-            Update
+            Add
           </Button>
           <br />
           <br />
@@ -262,4 +143,4 @@ const UpdateEvent = () => {
   );
 };
 
-export default UpdateEvent;
+export default AddNotification;

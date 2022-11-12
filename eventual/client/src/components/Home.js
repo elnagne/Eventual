4ac57ed6789f204a756172eb07c20fb4fce2588b
Jeremy La <jeremylai511@gmail.com>
2022-11-11 19:57:50 -0500

    RUN-104: Added dark mode for events page

--- eventual/client/src/components/Home.js
+++ eventual/client/src/components/Home.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import { useContext } from 'react'
 import SidebarPro from './SidebarPro'
+import { ThemeContext } from "./ThemeContext";
 
 const Home = () => {
+  const {theme} = useContext(ThemeContext);
+
   return (
     <div className="homeWrapper">
         <SidebarPro/>
-        <div className="homeContent">
+        <div className="homeContent" data-theme={theme}>
             <h1>Home</h1>
         </div>
     </div>
